aa74269bfa0359e69b2ee2c860e800e8a7a3f8f3
Bartłomiej Zając <bartoss97@gmail.com>
2019-04-14 00:17:28 +0200

    Added some docs

--- round-pong/src/BufferLayout.h
+++ round-pong/src/BufferLayout.h
@@ -1,37 +1,56 @@
 #pragma once
 
 
-struct BufferLayoutElement
-{
-    unsigned int count;
-    GLenum type;
-    GLboolean normalized;
-
-    BufferLayoutElement(unsigned int count, GLenum type, GLboolean normalized)
-        : count(count), type(type), normalized(normalized) {}
-};
-
-
+/**
+ * Class for representing vertex attributes in vertex buffer
+ */
 class BufferLayout
 {
 public:
-    static unsigned int getSizeOfGLtype(GLenum type);
+    /**
+     * Nested public class that represent single element in layout:
+     * that is variables in given 'type' in the amount of 'count',
+     * that represent one logical elment in buffer;
+     * eg. 3D vertex, that'd be 3 floats which form one BufferLayoutElement
+     */
+    struct BufferLayoutElement
+    {
+        unsigned int count;
+        GLenum type;
+        GLboolean normalized;
+
+        BufferLayoutElement(unsigned int count, GLenum type, GLboolean normalized)
+            : count(count), type(type), normalized(normalized)
+        {}
+    };
 
     BufferLayout()
-        : m_stride(0) 
+        : m_stride(0)
     {}
 
-    const std::vector<BufferLayoutElement>& getElements() const
+    const std::vector<BufferLayout::BufferLayoutElement>& getElements() const
     {
         return m_elements;
     }
 
+    /**
+     * @param type of variable represented as GLenum (unsigned int)
+     * @return size in bytes of a single variable of given type
+     */
+    static unsigned int getSizeOfGLtype(GLenum type);
+
+    /**
+     * @return size in bytes between one set of elements in vertex buffer and next one
+     */
     unsigned int getStride() const
     {
         return m_stride;
     }
 
-
+    /**
+     * Add to laypout variables of type T in the amount of count,
+     * that represent one logical element in vertex buffer
+     */
     template<typename T>
     void add(unsigned int count)
     {
